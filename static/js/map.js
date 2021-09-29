91c0d770eeb415e4e0247561069832c95b788c7f
Theodore Moreland <theodoremoreland@yahoo.com>
2021-09-28 22:45:43 -0500

    updated min zoom for map

--- static/js/map.js
+++ static/js/map.js
@@ -93,13 +93,13 @@ var stateLayer = L.layerGroup(stateMarkers);
 //Create title layers for light and dark maps
 var darkMap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
     maxZoom: 5.5,
-    minZoom: 4.5,
+    minZoom: 3,
     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
 
 var lightMap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
     maxZoom: 5.5,
-    minZoom: 4.5,
+    minZoom: 3,
     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
 
