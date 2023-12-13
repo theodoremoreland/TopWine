7206c4dbaf79b225430cbcd43858b45f48d1b92d
theodoremoreland <theodoremoreland@yahoo.com>
2023-12-12 22:59:17 -0600

    imporved plotly graph responsiveness

--- application/static/js/barGraph.js
+++ application/static/js/barGraph.js
@@ -5,66 +5,60 @@ var avg_price = [];
 var avg_rating = [];
 
 for (var index in Top10.grape) {
-    var variety = Top10.grape[index];
-    if (!varieties.includes(variety)) {
-        varieties.push(variety);
+  var variety = Top10.grape[index];
+  if (!varieties.includes(variety)) {
+    varieties.push(variety);
+  }
+}
+
+var sum = 0;
+var count = 0;
+
+for (var i in varieties) {
+  for (var j in Top10.price) {
+    if (Top10.grape[j] == varieties[i]) {
+      count += 1;
+      sum += parseInt(Top10.price[j]);
     }
-};
+  }
+
+  avg_price.push(sum / count);
+}
 
 var sum = 0;
 var count = 0;
 
 for (var i in varieties) {
+  for (var j in Top10.points) {
+    if (Top10.grape[j] == varieties[i]) {
+      count += 1;
+      sum += parseInt(Top10.points[j]);
+    }
+  }
 
-    for (var j in Top10.price) {
-        
-        if (Top10.grape[j] == varieties[i]) {
-            count += 1;
-            sum += parseInt(Top10.price[j]);
-        }
-        
-    };
-
-    avg_price.push(sum/count);
-};
-
-var sum = 0;
-var count = 0;
-
-for (var i in varieties) {
-
-    for (var j in Top10.points) {
-        
-        if (Top10.grape[j] == varieties[i]) {
-            count += 1;
-            sum += parseInt(Top10.points[j]);
-        }
-        
-    };
-    
-    avg_rating.push(sum/count);
-};
+  avg_rating.push(sum / count);
+}
 
 var trace1 = {
-    x: varieties,
-    y: avg_price,
-    name: "Average Price",
-    type: "bar",
+  x: varieties,
+  y: avg_price,
+  name: "Average Price",
+  type: "bar",
 
-    marker: {color: 'rgb(0,250,154)'}
-  };
+  marker: { color: "rgb(0,250,154)" },
+};
 
 var trace2 = {
-    x: varieties,
-    y: avg_rating,
-    name: "Average Rating",
-    type: "bar",
+  x: varieties,
+  y: avg_rating,
+  name: "Average Rating",
+  type: "bar",
 
-    marker: {color: 'rgb(128,0,128)'}
-  };
+  marker: { color: "rgb(128,0,128)" },
+};
 
-  var data = [trace1, trace2];
+var data = [trace1, trace2];
 
-  var layout = {barmode: 'group', };
-  
-  Plotly.newPlot("bar-chart", data, layout);
+var layout = { barmode: "group" };
+
+Plotly.newPlot("bar-chart", data, layout, { responsive: true });
--- application/static/js/scatterPlot.js
+++ application/static/js/scatterPlot.js
@@ -1,71 +1,75 @@
-
-function buildCharts(min, max){ 
-    // The dataset we are going to be manipulating
-    var wine = data10;
-    // then use the max and min values to give only the most useful of data
-    var winemin = [];
-    wine.forEach(function(w){
-        if(w.price >= min) {
-            winemin.push(w);
-        }
-    });
-    var winerange = [];
-    winemin.forEach(function(w){
-        if(w.price <= max) {
-            winerange.push(w);
-        }
-    });
-
-    // Build a scatter
-    // function buildScatter(bottleData) {
-    var scatterData = [{
-        x: winerange.map(w => w.price),
-        y: winerange.map(w => w.points),
-        type: "scatter",
-        mode: "markers",
-        text: winerange.map(w => w.designation),
-        hoverinfo: "text",
-        marker: {
-            color: 'rgb(148,0,211)',
-            size: 18,
-            opacity: .2
-        },
-        }];
-
-    var scatterlayout = {
-        xaxis: {
-            title:{
-                text:"Price ($)",
-                size: 18}
-        },
-        yaxis: {
-            title:{
-                text:"Rating (points)",
-                size: 18}
-        }
+function buildCharts(min, max) {
+  // The dataset we are going to be manipulating
+  var wine = data10;
+  // then use the max and min values to give only the most useful of data
+  var winemin = [];
+  wine.forEach(function (w) {
+    if (w.price >= min) {
+      winemin.push(w);
     }
-    Plotly.newPlot("scatter", scatterData, scatterlayout);
-};
+  });
+  var winerange = [];
+  winemin.forEach(function (w) {
+    if (w.price <= max) {
+      winerange.push(w);
+    }
+  });
 
+  // Build a scatter
+  // function buildScatter(bottleData) {
+  var scatterData = [
+    {
+      x: winerange.map((w) => w.price),
+      y: winerange.map((w) => w.points),
+      type: "scatter",
+      mode: "markers",
+      text: winerange.map((w) => w.designation),
+      hoverinfo: "text",
+      marker: {
+        color: "rgb(148,0,211)",
+        size: 18,
+        opacity: 0.2,
+      },
+    },
+  ];
 
+  var scatterlayout = {
+    xaxis: {
+      title: {
+        text: "Price ($)",
+        size: 18,
+      },
+    },
+    yaxis: {
+      title: {
+        text: "Rating (points)",
+        size: 18,
+      },
+    },
+  };
+  Plotly.newPlot("scatter", scatterData, scatterlayout, { responsive: true });
+}
 
 function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-    
-    // Use the first sample from the list to build the initial plots
-    const firstmin = 0;
-    const firstmax = 100000;
-    buildCharts(firstmin, firstmax);
-    // buildMetadata(firstmin, firstmax);
-};
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the first sample from the list to build the initial plots
+  const firstmin = 0;
+  const firstmax = 100000;
+
+  buildCharts(firstmin, firstmax);
+  // buildMetadata(firstmin, firstmax);
+}
+
 init();
 
 // Trying to get the new ranges when someone selects it on the scatter plot
 function optionChanged(selectObject) {
-    var ranges = selectObject.value;
-    var maxmin = ranges.split(',')
-    var newmin = maxmin[0];
-    var newmax = maxmin[1];
-    buildCharts(newmin, newmax);
-};
+  var ranges = selectObject.value;
+  var maxmin = ranges.split(",");
+  var newmin = maxmin[0];
+  var newmax = maxmin[1];
+
+  buildCharts(newmin, newmax);
+}
